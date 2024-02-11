40f1eadbebd4ae5757391b8c047ea37cc7970304
Yogie Yadav <yogiejay21@gmail.com>
2024-02-11 23:44:02 +0530

    adding new database

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
-import { db } from "./config-firebase";
+import { db, db1 } from "./config-firebase";
 import {
   collection,
   getDocs,
@@ -14,20 +14,51 @@ function App() {
   const [name, setName] = useState();
   const [age, setAge] = useState();
   const [user, setUsers] = useState([]);
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
   const UsersCollectionRef = collection(db, "backenddata");
+  const UserLoginData = collection(db, "backenddata");
 
   const CreateUser = async () => {
     await addDoc(UsersCollectionRef, { Name: name, Age: age });
+    await addDoc(UserLoginData, { Username: username, Password: password });
+    // setUsers((prev) => [...prev, { Name: name, Age: age }]);
     window.location.reload();
   };
+  // const CreateLoginCred = async () => {
+  //   await addDoc(UserLoginData, { Username: username, Password: password });
+  //   window.location.reload();
+  // };
   useEffect(() => {
     const getUsersData = async () => {
       const data = await getDocs(UsersCollectionRef);
       // setUsers(data.doc);
-      setUsers(data.docs.map((elem) => ({ ...elem.data(), id: elem.id })));
+      console.log(typeof data);
+      setUsers(
+        data.docs.map((elem) => {
+          return { ...elem.data(), id: elem.id };
+        })
+      );
+      // setUsers(data.docs);
     };
     getUsersData();
   }, []);
+
+  const increaseAge = async (id, age) => {
+    const userDoc = doc(db, "backenddata", id);
+    const NewAge = { Age: Number(age) + 1 };
+    console.log("Updated the Data on System");
+    await updateDoc(userDoc, NewAge);
+    console.log("Updated the Data on the Server");
+    window.location.reload();
+  };
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "backenddata", id);
+    console.log("Updated the Data on System");
+    await deleteDoc(userDoc);
+    console.log("Updated the Data on the Server");
+    window.location.reload();
+  };
 
   return (
     <div className="App">
@@ -58,6 +89,25 @@ function App() {
             }}
           />
           <br />
+          <span>Enter your Usrname : </span>
+          <input
+            className="mx-4 text-black"
+            type="text"
+            placeholder="Age"
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <span>Enter your Password : </span>
+          <input
+            className="mx-4 text-black"
+            type="text"
+            placeholder="Age"
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+          <br />
           <button
             onClick={CreateUser}
             className="bg-slate-700 m-4 p-2 w-20 rounded-md"
@@ -74,20 +124,13 @@ function App() {
               <div className="hover:animate-pulse m-4 bg-gray-600 w-1/4 rounded-md p-2">
                 <p className="w-auto text-center">{user.Name}</p>
                 <p className="w-auto text-center">{user.Age}</p>
-                {/* <button
-                  onClick={() => {
-                    increaseAge(user.id, user.age);
-                  }}
-                >
-                  Increase Age
-                </button>
                 <button
                   onClick={() => {
                     deleteUser(user.id);
                   }}
                 >
                   Delete User
-                </button> */}
+                </button>
               </div>
             );
           })}
--- src/config-firebase.js
+++ src/config-firebase.js
@@ -17,4 +17,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const db1 = getFirestore(app);
 // export default db;
